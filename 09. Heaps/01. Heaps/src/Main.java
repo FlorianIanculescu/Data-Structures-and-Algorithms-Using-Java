8b84f64af769993d7942af391fc190b48ff166a1
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2023-09-05 22:16:11 +0300

    Add files via upload

---
+++ 09. Heaps/01. Heaps/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Heap heap = new Heap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+//        System.out.println("-".repeat(40));
+//        heap.printHeap();
+//
+        heap.delete(0);
+
+        System.out.println("-".repeat(40));
+        heap.printHeap();
+
+        System.out.println("-".repeat(40));
+        System.out.println(heap.peek());
+    }
+}
